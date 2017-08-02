a7cfe8bf6c2ab0c17197b4e2ff6593200a99b8e5
Nazmul Hasan <nazmuls4@yahoo.com>
2017-08-02 06:27:22 -0700

    class-53
    class-53

---
+++ class-53/assets/js/active.js
@@ -0,0 +1,40 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+	   
+        
+        
+        //portfolio
+        $(".cat-sider").owlCarousel({
+            items: 3, // Default is 3
+            loop: true,
+            autoplay: false,
+            autoplayTimeout: 3000, // Default is 5000
+            smartSpeed: 1000, // Default is 250
+            dots: true,
+            nav: false,
+            margin:30,
+        });
+        
+        
+        
+        
+        
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+		
+
+    });
+
+
+
+
+}(jQuery));
