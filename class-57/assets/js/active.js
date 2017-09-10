903049e53457550e8cbc07bf6ba66792eeea1733
Nazmul Hasan <nazmuls4@yahoo.com>
2017-09-10 12:40:28 +0600

    57

---
+++ class-57/assets/js/active.js
@@ -0,0 +1,23 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+	           
+       $('.testimonial-author-name').perfectScrollbar(); 
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+		
+
+    });
+
+
+
+
+}(jQuery));
