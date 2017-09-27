c85fca1355010010bceb59ba008aad3bf1724d66
Nazmul Hasan <nazmuls4@yahoo.com>
2017-09-27 11:23:55 +0600

    rrrsdgs

---
+++ class-69/assets/js/active.js
@@ -0,0 +1,24 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+	           
+        
+			var footerHeight = $(".footer-area").height();
+			$(".hole-site-wrapper").css('margin-bottom', footerHeight + 'px');
+
+    });
+
+	
+
+    jQuery(window).load(function() {
+		
+    });
+
+
+
+
+}(jQuery));
